6ea7e660a2b994de9adad8aa9bec02bfe784c80f
Jimmy Iovine <jiovinejr@gmail.com>
2024-07-12 18:35:25 -0400

    Adjusting tables to take advantage of references in DB

--- delaware_ship_db.sql
+++ delaware_ship_db.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS name_clean_up CASCADE;
 DROP TABLE IF EXISTS measurement_clean_up CASCADE;
+DROP TABLE IF EXISTS ship CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 
 CREATE TABLE name_clean_up (
@@ -8,21 +9,35 @@ CREATE TABLE name_clean_up (
 	clean_item_name varchar(100),
 	category varchar(50),
 	case_weight numeric(5,2)
+
+	CONSTRAINT pk_name_clean_up PRIMARY KEY (item_id)
 );
 
 CREATE TABLE measurement_clean_up (
-	item_id serial PRIMARY KEY,
+	measurement_id serial PRIMARY KEY,
 	original_measurement_name varchar(100),
 	clean_measurement_name varchar(100)
+
+	CONSTRAINT pk_measurement_clean_up PRIMARY KEY (measurement_id)
 );
 
+CREATE TABLE ship(
+    ship_id serial PRIMARY KEY,
+    ship_name varchar(100)
+
+    CONSTRAINT pk_ship PRIMARY KEY (ship_id)
+);
+
+
 CREATE TABLE orders (
-	order_item_id serial PRIMARY KEY,
-	ship_name varchar(100),
+	order_id serial PRIMARY KEY,
+	order_ship_id int NOT NULL,
 	quantity numeric(5,2),
-	original_measurement_name varchar(100),
-    clean_measurement_name varchar(100),
-	original_item_name varchar(100),
-	clean_item_name varchar(100),
-	case_weight numeric(5,2)
+    order_measurement_id int NOT NULL,
+    order_item_id int NOT NULL,
+
+    CONSTRAINT pk_orders PRIMARY KEY (order_id)
+    CONSTRAINT fk_order_ship FOREIGN KEY (order_ship_id) REFERENCES ship (ship_id)
+    CONSTRAINT fk_order_measurement FOREIGN KEY (order_measurement_id) REFERENCES measurement_clean_up (measurement_id)
+    CONSTRAINT fk_order_item FOREIGN KEY (order_item_id) REFERENCES name_clean_up (item_id)
 );
